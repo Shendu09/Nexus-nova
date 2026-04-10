14cfc62c43ccf7b8ec0ca750d8d4acccc013bff2
shendu09 <alyy1069@gmail.com>
2026-04-11 01:37:03 +0530

    Add: temp_commit.ps1

---
+++ temp_commit.ps1
@@ -0,0 +1,9 @@
+﻿# Function to commit files individually
+$files = Get-ChildItem -File -Recurse | Where-Object {$_.FullName -notmatch '\.git'} | Sort-Object FullName
+
+foreach ($file in $files) {
+    $relativePath = $file.FullName -replace [regex]::Escape($PWD), ""
+    $relativePath = $relativePath.TrimStart('\')
+    git add "$relativePath"
+    git commit -m "Add: $relativePath" 2>&1 | Select-Object -First 1
+}
