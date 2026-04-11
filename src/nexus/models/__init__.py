b16181a7fb3fb3d1b477c72e719f097638242980
shendu09 <alyy1069@gmail.com>
2026-04-11 07:32:28 +0530

    ml: export BERT classifier from models package

--- src/nexus/models/__init__.py
+++ src/nexus/models/__init__.py
@@ -8,12 +8,31 @@ This package contains deep learning models for:
 """
 
 from .autoencoder import LogAutoencoder
-from .lstm import LogAnomalyLSTM, LogAnomalyLSTMConfig, LogSequenceDataset, LogAnomalyLSTMScorer
+from .bert_classifier import (
+    SeverityClassifier,
+    SeverityClassifierConfig,
+    SeverityClassifierTrainer,
+    SeverityPrediction
+)
+
+try:
+    from .lstm import LogAnomalyLSTM, LogAnomalyLSTMConfig, LogSequenceDataset, LogAnomalyLSTMScorer
+    _lstm_available = True
+except ImportError:
+    _lstm_available = False
 
 __all__ = [
     "LogAutoencoder",
-    "LogAnomalyLSTM",
-    "LogAnomalyLSTMConfig",
-    "LogSequenceDataset",
-    "LogAnomalyLSTMScorer",
+    "SeverityClassifier",
+    "SeverityClassifierConfig",
+    "SeverityClassifierTrainer",
+    "SeverityPrediction",
 ]
+
+if _lstm_available:
+    __all__.extend([
+        "LogAnomalyLSTM",
+        "LogAnomalyLSTMConfig",
+        "LogSequenceDataset",
+        "LogAnomalyLSTMScorer",
+    ])
